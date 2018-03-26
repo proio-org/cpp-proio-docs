655f9b141c1c31a1ca5c44080aa4db02f97ec6dc
Travis CI User <travis@example.org>
2018-03-26 23:31:50 +0000

    Autogenerated via decibelcooper/proio Travis CI build: 511
    Commit: 7b575b9ae614aec0d739610ffdf1060e9116370a

--- search/all_c.js
+++ search/all_c.js
@@ -3,7 +3,8 @@ var searchData=
   ['seekerror',['SeekError',['../classproio_1_1SeekError.html',1,'proio']]],
   ['seektostart',['SeekToStart',['../classproio_1_1Reader.html#a841aff8caf5ca4436555daad3b24a57b',1,'proio::Reader']]],
   ['serializationerror',['SerializationError',['../classproio_1_1SerializationError.html',1,'proio']]],
-  ['setcompression',['SetCompression',['../classproio_1_1Writer.html#a039de7907698bc83082294abb43382c1',1,'proio::Writer']]],
+  ['setbucketdumpthreshold',['SetBucketDumpThreshold',['../classproio_1_1Writer.html#a1e0b8b3811dc0ec6ea68c7d4c964d9b6',1,'proio::Writer']]],
+  ['setcompression',['SetCompression',['../classproio_1_1Writer.html#ae5bdb7597143651776fe8616763075d6',1,'proio::Writer']]],
   ['skip',['Skip',['../classproio_1_1Reader.html#afd1326802327d7f4241188c4eb8a7b4f',1,'proio::Reader']]],
   ['string',['String',['../classproio_1_1Event.html#a5f105a5b3daddc3a893a44b8de445819',1,'proio::Event']]]
 ];
--- search/classes_9.js
+++ search/classes_9.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['writejob',['WriteJob',['../structproio_1_1Writer_1_1WriteJob.html',1,'proio::Writer']]],
   ['writer',['Writer',['../classproio_1_1Writer.html',1,'proio']]]
 ];
