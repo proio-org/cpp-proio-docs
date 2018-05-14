bc6637d4a25f6836a50ede1289d23f9ee302031b
Travis CI User <travis@example.org>
2018-05-14 18:53:30 +0000

    Autogenerated via decibelcooper/proio Travis CI build: 591
    Commit: 608caed02fa9e7ccdb86c7458cf1141f9b42c081

--- search/functions_9.js
+++ search/functions_9.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['seektostart',['SeekToStart',['../classproio_1_1Reader.html#a841aff8caf5ca4436555daad3b24a57b',1,'proio::Reader']]],
   ['setbucketdumpthreshold',['SetBucketDumpThreshold',['../classproio_1_1Writer.html#a1e0b8b3811dc0ec6ea68c7d4c964d9b6',1,'proio::Writer']]],
-  ['setcompression',['SetCompression',['../classproio_1_1Writer.html#ae5bdb7597143651776fe8616763075d6',1,'proio::Writer']]],
+  ['setcompression',['SetCompression',['../classproio_1_1Writer.html#a069a50d1c5a528b8c7b6bcffce71461c',1,'proio::Writer']]],
   ['skip',['Skip',['../classproio_1_1Reader.html#afd1326802327d7f4241188c4eb8a7b4f',1,'proio::Reader']]],
   ['string',['String',['../classproio_1_1Event.html#a5f105a5b3daddc3a893a44b8de445819',1,'proio::Event']]]
 ];
