7f4ddaae49214ffc5c4c8d6fbaa9041f513d40db
Travis CI User <travis@example.org>
2018-06-03 17:34:11 +0000

    Autogenerated via decibelcooper/proio Travis CI build: 630
    Commit: 912c5aa0f0ee838d5ace080b0d58a31a2a409ead

--- search/all_5.js
+++ search/all_5.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['filecreationerror',['FileCreationError',['../classproio_1_1FileCreationError.html',1,'proio']]],
   ['fileopenerror',['FileOpenError',['../classproio_1_1FileOpenError.html',1,'proio']]],
-  ['flush',['Flush',['../classproio_1_1Writer.html#aa66ca9fb97a5439b7d97c1a116da4090',1,'proio::Writer']]]
+  ['flush',['Flush',['../classproio_1_1Writer.html#aa66ca9fb97a5439b7d97c1a116da4090',1,'proio::Writer']]],
+  ['free',['Free',['../classproio_1_1Event.html#a70cbedb7555e195aa3405d7beba9cb84',1,'proio::Event']]]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['flush',['Flush',['../classproio_1_1Writer.html#aa66ca9fb97a5439b7d97c1a116da4090',1,'proio::Writer']]]
+  ['flush',['Flush',['../classproio_1_1Writer.html#aa66ca9fb97a5439b7d97c1a116da4090',1,'proio::Writer']]],
+  ['free',['Free',['../classproio_1_1Event.html#a70cbedb7555e195aa3405d7beba9cb84',1,'proio::Event']]]
 ];
