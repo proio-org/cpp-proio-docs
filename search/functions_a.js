44a4cad8e5582bc48a5bc0b043ddffdd85ac127a
Travis CI User <travis@example.org>
2018-10-15 22:18:31 +0000

    Autogenerated via cpp-proio Travis CI build: 73
    Commit: f0b989d55da5d8aba8ae35e096e729fdf0d21814

--- search/functions_a.js
+++ search/functions_a.js
@@ -3,7 +3,7 @@ var searchData=
   ['seektostart',['SeekToStart',['../classproio_1_1Reader.html#a841aff8caf5ca4436555daad3b24a57b',1,'proio::Reader']]],
   ['serializetostring',['SerializeToString',['../classproio_1_1Event.html#ad431d4416636e8b4aca9e52c8a47e6af',1,'proio::Event']]],
   ['setbucketdumpthreshold',['SetBucketDumpThreshold',['../classproio_1_1Writer.html#a1e0b8b3811dc0ec6ea68c7d4c964d9b6',1,'proio::Writer']]],
-  ['setcompression',['SetCompression',['../classproio_1_1Writer.html#a069a50d1c5a528b8c7b6bcffce71461c',1,'proio::Writer']]],
+  ['setcompression',['SetCompression',['../classproio_1_1Writer.html#a3acfa35de7236610c47ec78173bb8122',1,'proio::Writer']]],
   ['setdescriptorpool',['SetDescriptorPool',['../classproio_1_1Event.html#adc674fb22935a805c530b323696882db',1,'proio::Event']]],
   ['skip',['Skip',['../classproio_1_1Reader.html#afd1326802327d7f4241188c4eb8a7b4f',1,'proio::Reader']]],
   ['string',['String',['../classproio_1_1Event.html#a5f105a5b3daddc3a893a44b8de445819',1,'proio::Event']]]
